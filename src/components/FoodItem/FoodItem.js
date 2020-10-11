795b70003caea9c8308668235978aedb476e0e23
nahid <nahidkishore99@gmail.com>
2020-10-12 00:28:10 +0600

    card shadow and margin added

--- src/components/FoodItem/FoodItem.js
+++ src/components/FoodItem/FoodItem.js
@@ -10,9 +10,9 @@ const FoodItem = (props) => {
     history.push(`/item/${id}`);
   };
   return (
-    <div className="col-md-4 mb-2">
-      <div className="card px-2 shadow" onClick={handleDetails}>
-        <img src={picture} alt="" className="img-fluid" width="100%" />
+    <div className="col-md-4  d-flex justify-content-between">
+      <div className="card px-2 shadow mb-5" onClick={handleDetails}>
+        <img src={picture} alt="" className="img-fluid ml-5 " width="80%" />
         <div className="card-body text-center">
           <div className="card-title">
             <p className="card-text">{name}</p>
